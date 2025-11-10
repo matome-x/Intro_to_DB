ff45cd2c051a642c758a8baf760d72d0fcf6a6a5
Matome Mahlatsi Modiba <geniustoddler@gmail.com>
2025-11-10 04:10:55 +0000

    full description of the 'books' table

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Print full description of the 'Books' table
+SELECT 
+    COLUMN_NAME AS `Column`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Nullable`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
